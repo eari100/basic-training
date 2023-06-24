1db7a7ec78bcb391b2a6e466695c7baeab165827
Jaewook Choi <eari100@naver.com>
2023-06-24 10:42:12 +0900

    refactor: Todo.tsx

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -37,38 +37,44 @@ export const Todo = () => {
         setEditIndex(null);
     }
 
+    const handleEditChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (editTodoRef.current) {
+            editTodoRef.current.value = e.target.value;
+        }
+    };
+
+    const renderDefaultItem = (todo: string, index: number) => {
+        return (
+            <div key={index}>
+                <li>{todo}</li>
+                <button onClick={() => startEditing(index)}>수정</button>
+                <button onClick={() => deleteTodo(index)}>삭제</button>
+            </div>
+        )
+    }
+
+    const renderEditingItem = (todo: string, index: number) => {
+        return (
+            <div key={index}>
+                <input
+                    type="text"
+                    defaultValue={todo}
+                    ref={editTodoRef}
+                    onChange={ (e) => handleEditChange }
+                />
+                <button onClick={() => editTodo(index)}>완료</button>
+                <button onClick={cancelEditing}>취소</button>
+            </div>
+        )
+    }
+
     return (
         <>
             <input type="text" ref={newTodoRef} />
             <button onClick={addTodo}>추가</button>
             {
                 todoList.map((todo, index) => {
-                    if(editIndex !== index) {
-                        return (
-                        <div key={index}>
-                            <li>{todo}</li>
-                            <button onClick={() => startEditing(index)}>수정</button>
-                            <button onClick={() => deleteTodo(index)}>삭제</button>
-                        </div>
-                        )
-                    } else {
-                        return (
-                            <div key={index}>
-                                <input
-                                    type="text"
-                                    defaultValue={todo}
-                                    ref={editTodoRef}
-                                    onChange={(e) => {
-                                        if (editTodoRef.current) {
-                                            editTodoRef.current.value = e.target.value;
-                                        }
-                                    }}
-                                />
-                                <button onClick={() => editTodo(index)}>완료</button>
-                                <button onClick={cancelEditing}>취소</button>
-                            </div>
-                        )
-                    }
+                    return editIndex !== index ? renderDefaultItem(todo, index) : renderEditingItem(todo, index)
                 })
             }
         </>
