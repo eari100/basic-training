ffbf4bb085db2ee4f7ae4ab222921996af3f2dba
Jaewook Choi <eari100@naver.com>
2023-06-26 23:56:42 +0900

    test: Todo 컴포넌트 추가, 수정, 삭제 jest.Mock 으로 api 테스트

--- src/Todo.test.tsx
+++ src/Todo.test.tsx
@@ -1,60 +1,103 @@
-import React from "react";
-import { render, fireEvent, screen } from "@testing-library/react";
-import { Todo } from "./Todo";
+import { render, screen, fireEvent } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import { Todo } from './Todo';
+import { getTodos, saveTodos, patchTodos, deleteTodos } from './api/todosApi';
 
-describe("Todo 컴포넌트 테스트", () => {
-    test("할 일 추가하기", () => {
-        render(<Todo />);
+jest.mock('./api/todosApi');
 
-        const addButton = screen.getByText("추가");
-        const input = screen.getByRole("textbox");
-
-        fireEvent.change(input, { target: { value: "새로운 할 일" } });
-        fireEvent.click(addButton);
-
-        const todoItem = screen.getByText("새로운 할 일");
-        expect(todoItem).toBeInTheDocument();
+describe('Todo 컴포넌트', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
     });
 
-    test("할 일 수정하기", () => {
+    test('할 일 추가하기', async () => {
+        const todosData = [
+            { id: 1, item: '할 일 1' },
+            { id: 2, item: '할 일 2' },
+        ];
+
+        (getTodos as jest.Mock).mockResolvedValue(todosData);
+        (saveTodos as jest.Mock).mockResolvedValue(undefined);
+
         render(<Todo />);
 
+        const newTodoInput = screen.getByRole("textbox");
         const addButton = screen.getByText("추가");
-        const addInput = screen.getByRole("textbox");
 
-        fireEvent.change(addInput, { target: { value: "할 일 1" } });
+        fireEvent.change(newTodoInput, { target: { value: '새로운 할 일' } });
         fireEvent.click(addButton);
-        fireEvent.click(screen.getByText("수정"));
 
-        const editButton = screen.getByText("완료");
-        const editInput = screen.getAllByRole("textbox")[1];
+        const updatedTodosData = [
+            ...todosData,
+            { id: 3, item: '새로운 할 일' },
+        ];
 
-        fireEvent.change(editInput, { target: { value: "수정된 할 일 1" } });
+        (getTodos as jest.Mock).mockResolvedValue(updatedTodosData);
+
+        expect(saveTodos).toHaveBeenCalledWith('새로운 할 일');
+        expect(getTodos).toHaveBeenCalledTimes(1);
+
+        await screen.findByText('할 일 1');
+        await screen.findByText('할 일 2');
+        expect(screen.getByText('새로운 할 일')).toBeInTheDocument();
+    });
+
+
+    test('할 일 수정하기', async () => {
+        const todosData = [{ id: 1, item: '할 일' }];
+        (getTodos as jest.Mock).mockResolvedValue(todosData);
+        (patchTodos as jest.Mock).mockResolvedValue(undefined);
+
+        render(<Todo />);
+
+        await screen.findByText('수정');
+        const editButton = screen.getByText("수정");
         fireEvent.click(editButton);
 
-        const updatedItem = screen.getByText("수정된 할 일 1");
-        expect(updatedItem).toBeInTheDocument();
+        const editInput = screen.getAllByRole("textbox")[1];
+
+        await screen.findByText('완료');
+        const doneButton = screen.getByText("완료");
+
+        fireEvent.change(editInput, { target: { value: '수정된 할 일' } });
+        fireEvent.click(doneButton);
+
+        const updatedTodosData = [
+            { id: 1, item: '수정된 할 일' },
+        ];
+
+        (getTodos as jest.Mock).mockResolvedValue(updatedTodosData);
+
+        expect(patchTodos).toHaveBeenCalledWith(1, '수정된 할 일');
+        expect(getTodos).toHaveBeenCalledTimes(1);
+
+        await screen.findByText('수정된 할 일');
+        expect(screen.getByText('수정된 할 일')).toBeInTheDocument();
     });
 
-    test("할 일 삭제하기", () => {
+    test('할 일 삭제하기', async () => {
+        const todosData = [{ id: 1, item: '할 일' }];
+        (getTodos as jest.Mock).mockResolvedValue(todosData);
+        (deleteTodos as jest.Mock).mockResolvedValue(undefined);
+        window.confirm = jest.fn(() => true);
+
         render(<Todo />);
 
-        const addButton = screen.getByText("추가");
-        const input = screen.getByRole("textbox");
-
-        fireEvent.change(input, { target: { value: "할 일 1" } });
-        fireEvent.click(addButton);
-
-        fireEvent.change(input, { target: { value: "할 일 2" } });
-        fireEvent.click(addButton);
-
-        const deleteButton = screen.getAllByText("삭제")[0];
+        await screen.findByText('삭제');
+        const deleteButton = screen.getByText('삭제');
         fireEvent.click(deleteButton);
+        expect(window.confirm).toHaveBeenCalledWith('정말로 삭제하시겠습니까?');
 
-        const deletedItem = screen.queryByText("할 일 1");
-        expect(deletedItem).toBeNull();
+        const updatedTodosData = [{}];
 
-        const todoItem = screen.queryByText("할 일 2");
-        expect(todoItem).toBeInTheDocument();
+        (getTodos as jest.Mock).mockResolvedValue(updatedTodosData);
+
+        expect(deleteTodos).toHaveBeenCalledWith(1);
+        expect(getTodos).toHaveBeenCalledTimes(1);
+
+        // eslint-disable-next-line testing-library/no-unnecessary-act
+        await act(async () => {
+            await screen.findByText('할 일', { exact: false });
+        });
     });
 });
